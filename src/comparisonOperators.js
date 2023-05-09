8ad8ec2fa5875f182319115ad3a28b45acf02e5b
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-05-09 15:05:15 +0300

    Remove redundant files

--- src/comparisonOperators.js
+++
@@ -1,3 +0,0 @@
-const comparisonOperators = ['=', '!=', '<', '>'];
-
-export default comparisonOperators;
--- src/studentsInfo.js
+++
@@ -1,9 +0,0 @@
-const studentsInfo = [
-  'First Name',
-  'Last Name',
-  'Age',
-  'Level',
-  'Enrollment Year',
-];
-
-export default studentsInfo;
