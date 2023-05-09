a0930fd62d67ffab137e1b52015057f2693b2230
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-05-09 14:39:05 +0300

    Refactor code to adhere to eslint rules

--- src/Rule.js
+++ src/Rule.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Rule.css';
 
-class Rule extends Component {
-  render() {
-    return (
-      <li>
-        <select
-          className="select-student-info"
-          onChange={this.props.onFieldChanged}
-        >
-          {this.props.field}
-        </select>
-        <select
-          className="select-comparison-operator"
-          onChange={this.props.onOperatorChanged}
-        >
-          {this.props.operator}
-        </select>
-        <input className="input-value" onChange={this.props.onValueChanged} />
-        <button className="btn-delete-rule" onClick={this.props.onDelete}>
-          DELETE
-        </button>
-      </li>
-    );
-  }
+function Rule({
+  onFieldChanged,
+  field,
+  onOperatorChanged,
+  operator,
+  onValueChanged,
+  onDelete,
+}) {
+  return (
+    <li>
+      <select className="select-student-info" onChange={onFieldChanged}>
+        {field}
+      </select>
+      <select
+        className="select-comparison-operator"
+        onChange={onOperatorChanged}
+      >
+        {operator}
+      </select>
+      <input className="input-value" onChange={onValueChanged} />
+      <button type="button" className="btn-delete-rule" onClick={onDelete}>
+        DELETE
+      </button>
+    </li>
+  );
 }
 
 Rule.propTypes = {
   onFieldChanged: PropTypes.func.isRequired,
-  field: PropTypes.array.isRequired,
-  operator: PropTypes.array.isRequired,
+  field: PropTypes.arrayOf(PropTypes.string).isRequired,
+  operator: PropTypes.arrayOf(PropTypes.string).isRequired,
   onOperatorChanged: PropTypes.func.isRequired,
   onValueChanged: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
